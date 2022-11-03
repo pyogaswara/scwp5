1ff38f96f9886832e1260674d62cfb500bf09b96
prihadi.yogaswara@gmail.com <63325373+pyogaswara@users.noreply.github.com>
2022-11-03 18:01:57 +0000

    merapikan

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,10 +1,8 @@
 class Hat:
     def __init__(self,**kwargs):
         self.contents=[]
-        panjang=len(kwargs)
-        print(kwargs)
-        print(panjang)
-        print(type(kwargs))
-        print(dir(kwargs))
+       
+        
+
     def draw(self,number_of_balls_drawed):
         pass
