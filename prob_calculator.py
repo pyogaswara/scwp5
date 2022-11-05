20702a302c88123a91c3c4b9ecb281ba3fe0eceb
prihadi.yogaswara@gmail.com <63325373+pyogaswara@users.noreply.github.com>
2022-11-05 15:49:42 +0000

    semua print, kecuali hasil, dihilangkan

--- prob_calculator.py
+++ prob_calculator.py
@@ -7,63 +7,45 @@ class Hat:
       self.contents_keys=[]
       self.contents_values=[]
       self.drawn_balls=[]
-    # print(kwargs)
       for keys in kwargs.keys():
         self.contents_keys.append(keys)
       for keys in kwargs.keys():
         ball_multiplier=kwargs[keys]
         for ball in range (0,ball_multiplier):
             self.contents.append(keys)
-    #  print(self.contents)
       
     def draw(self,number_of_balls_drawn):
       self.contents_copy=copy.deepcopy(self.contents)
       self.drawn_balls_copy=copy.deepcopy(self.drawn_balls)
       contents_size=len(self.contents_copy)
-      #self.contents_copy=copy.deepcopy(self.contents)
+
       if number_of_balls_drawn < (len(self.contents_copy)):
         for balls in range (0,number_of_balls_drawn):    
             pop_index=random.randint(0,contents_size-1)
             self.drawn_balls_copy.append(self.contents_copy.pop(pop_index))
             contents_size=contents_size-1
-        #print(self.drawn_balls_copy)
-        #print("bola di topi ada:",len(self.contents_copy),self.contents_copy)
         return self.drawn_balls_copy        
       else:
         self.drawn_balls_copy=self.contents_copy
         return self.drawn_balls_copy
-
     
 
 def experiment(**kwargs):
-
   hat_instance=kwargs["hat"]
   expected_balls=kwargs["expected_balls"]
   num_balls_drawn=kwargs["num_balls_drawn"]
   num_experiments=kwargs["num_experiments"]
-
-  
-  #print("diinginkan")
-  #print(expected_balls)
-  #print("yang terambil")
- 
-
   num_of_occurence=0
-
   for experiment in range (0,num_experiments):
-    #print("experiment : ",experiment+1)
     drawn_balls=hat_instance.draw(num_balls_drawn)
-    #print("jumlah bola diambil:",num_balls_drawn,drawn_balls)
     expected_balls_list=[]
     for color in expected_balls.keys():
       num_color=drawn_balls.count(color)
       num_expected_color=(expected_balls[color])
-    #  print(color,"ada",num_color,"padahal mintanya",num_expected_color)
       if num_color >= num_expected_color:
         expected_balls_list.append(1)
       else:
         expected_balls_list.append(0)
-    #print(expected_balls_list)
     if expected_balls_list.__contains__(0):
       num_of_occurence=num_of_occurence+0
     else:
