ea972988363a755d6dbae18a5e0f66a6ecc76d64
prihadi.yogaswara@gmail.com <63325373+pyogaswara@users.noreply.github.com>
2022-11-05 15:54:39 +0000

    sudah bisa ngotak atik experiment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import prob_calculator
 
-hat = prob_calculator.Hat(black=6, red=4, green=3)
+hat = prob_calculator.Hat(black=6, red=8, green=3)
 probability = prob_calculator.experiment(hat=hat,
-                  expected_balls={"red":2,"green":1},
+                  expected_balls={"red":1,"green":1},
                   num_balls_drawn=5,
                   num_experiments=2000)
